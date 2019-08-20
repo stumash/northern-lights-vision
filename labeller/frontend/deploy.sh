8018a924bfa7dc9a4d72cac03be8e8c0a1cb325b
stumash <stuart.mashaal@gmail.com>
2019-08-20 01:03:58 -0400

    delete s3 bucket before syncing to ensure cache-control is set to no-cache on all files

--- labeller/frontend/deploy.sh
+++ labeller/frontend/deploy.sh
@@ -6,6 +6,10 @@ THIS_DIR="$(dirname "$(readlink -f "${0}")")"
 cd "${THIS_DIR}"
 
 aws "${aws_profile_flag}" \
+    s3 rm 's3://labeller.northernlights.vision/' \
+    --recursive
+
+aws "${aws_profile_flag}" \
     s3 sync . s3://labeller.northernlights.vision \
     --delete \
     --cache-control no-cache
