b2a858652e3f54148052227d373d7b203eb8afdd
stumash <stuart.mashaal@gmail.com>
2019-08-20 00:53:41 -0400

    add "--cache-control no-cache" to deploy.sh

--- labeller/frontend/deploy.sh
+++ labeller/frontend/deploy.sh
@@ -5,4 +5,7 @@ aws_profile_flag="${1}" # e.g. --profile=john
 THIS_DIR="$(dirname "$(readlink -f "${0}")")"
 cd "${THIS_DIR}"
 
-aws "${aws_profile_flag}" s3 sync . s3://labeller.northernlights.vision --delete
+aws "${aws_profile_flag}" \
+    s3 sync . s3://labeller.northernlights.vision \
+    --delete \
+    --cache-control no-cache
