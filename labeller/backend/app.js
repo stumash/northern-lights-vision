6d4f7b579c3049d6627c61a7c7aa559a4795a95f
David Huculak <davidhuculak5@gmail.com>
2019-09-07 22:23:51 -0400

    Changes:     - Replace single quotes by double quotes     - Don't make save API call if no video is selected     - Support frame skipping via arrow keys

--- labeller/backend/app.js
+++ labeller/backend/app.js
@@ -1,20 +1,20 @@
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
 
-const _ = require('lodash')
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const {asyncHandler, validateAnnotations, s3utils} = require('./utils');
+const {asyncHandler, validateAnnotations, s3utils} = require("./utils");
 
 const app = express();
 const router = express.Router();
 
-const bucketUrl = 'data.northernlights.vision';
-const vidPrefix = 'unlabelled/';
-const annotPrefix = 'annotations/';
+const bucketUrl = "data.northernlights.vision";
+const vidPrefix = "unlabelled/";
+const annotPrefix = "annotations/";
 
-router.use(cors())
+router.use(cors());
 router.use(bodyParser.json());
 router.use(awsServerlessExpressMiddleware.eventContext());
 
@@ -22,7 +22,7 @@ router.use(awsServerlessExpressMiddleware.eventContext());
  * List the s3 object urls of all unlabelled video files. If the video file has a corresponding annotation
  * file, list the s3 object url of that file too. Sort the list by videoUrl.
  */
-router.get('/list', asyncHandler(async (req, res, next) => {
+router.get("/list", asyncHandler(async (req, res, next) => {
   let [videoUrls, annotationUrls] = await Promise.all([
     s3utils.getAllBucketKeys(bucketUrl, vidPrefix),
     s3utils.getAllBucketKeys(bucketUrl, annotPrefix)
@@ -35,7 +35,7 @@ router.get('/list', asyncHandler(async (req, res, next) => {
     {annotationUrl, annotationAuthor}
   ));
 
-  // gets the 'name' of a file from its s3 object url. (filename w/o extension)
+  // gets the "name" of a file from its s3 object url. (filename w/o extension)
   const filenameNoExtRegex = /[a-zA-Z0-9_-]+(?=\.[a-zA-Z0-9]+$)/g;
 
   const annsWithAuthsByName = _.keyBy(annsWithAuths, aa => aa.annotationUrl.match(filenameNoExtRegex)[0]);
@@ -50,11 +50,11 @@ router.get('/list', asyncHandler(async (req, res, next) => {
   }));
 }));
 
-router.post('/annotate', asyncHandler(async (req, res, next) => {
+router.post("/annotate", asyncHandler(async (req, res, next) => {
   const {videoPath, annotations, annotatedBy} = req.body;
   const annotationPath = videoPath
     .replace(vidPrefix, annotPrefix)
-    .replace('.mp4', '.json');
+    .replace(".mp4", ".json");
 
   validateAnnotations(annotations);
 
@@ -63,6 +63,6 @@ router.post('/annotate', asyncHandler(async (req, res, next) => {
   res.json({});
 }));
 
-app.use('/', router);
+app.use("/", router);
 
 module.exports = app;
