fee90983253de4e16d8606a95051fa645ac16f0d
stumash <stuart.mashaal@gmail.com>
2019-09-11 23:30:58 -0400

    terraform: add s3 public access blocks, add labeller bucket

--- main.tf
+++ main.tf
@@ -47,3 +47,42 @@ resource "aws_s3_bucket" "data" {
     allowed_headers = ["*"]
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "data" {
+  bucket = "${aws_s3_bucket.data.id}"
+  block_public_acls = true
+  ignore_public_acls = true
+  block_public_policy = false
+}
+
+resource "aws_s3_bucket" "labeller" {
+  bucket = "labeller.northernlights.vision"
+  region = "us-east-1"
+
+  website {
+    index_document = "index.html"
+  }
+
+  policy = <<-POLICY
+           {
+             "Version": "2012-10-17",
+             "Id": "Policy1566271355793",
+             "Statement": [
+               {
+                   "Sid": "Stmt1566271354288",
+                   "Effect": "Allow",
+                   "Principal": "*",
+                   "Action": "s3:GetObject",
+                   "Resource": "arn:aws:s3:::labeller.northernlights.vision/*"
+               }
+             ]
+           }
+           POLICY
+}
+
+resource "aws_s3_bucket_public_access_block" "labeller" {
+  bucket = "${aws_s3_bucket.labeller.id}"
+  block_public_acls = true
+  ignore_public_acls = true
+  block_public_policy = false
+}
